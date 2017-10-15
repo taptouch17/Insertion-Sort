2652e0540cd4cb584ac389f46ee1efb2c3e44611
Graeme Stoute <graemeboy@hotmail.com>
2017-10-15 14:50:02 -0400

    Initial Commit

---
+++ Insertion Sort/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Insertion Sort
+//
+//  Created by Graeme Stoute on 15/10/2017.
+//  Copyright © 2017 Graeme Stoute. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
