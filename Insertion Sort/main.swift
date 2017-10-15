3410c46c0f0974e9ac298b5379cdc901fa8b8afe
Graeme Stoute <graemeboy@hotmail.com>
2017-10-15 14:52:24 -0400

    Insertion Sort

--- Insertion Sort/main.swift
+++ Insertion Sort/main.swift
@@ -10,3 +10,18 @@ import Foundation
 
 print("Hello, World!")
 
+func insertionSort(_ array: [Int]) -> [Int] {
+    var a = array             // 1
+    for x in 1..<a.count {         // 2
+        var y = x
+        while y > 0 && a[y] < a[y - 1] { // 3
+            a.swapAt(y - 1, y)
+            y -= 1
+        }
+    }
+    return a
+}
+
+
+let array = [20,109,283,8495,1,9,5,6,7,4,3,2,6,28,21,43,24]
+print(insertionSort(array))
